3bda7e8e599b7b5cde7421b58a2117d0272df408
Michail Bellas <michail.bellas@eurodyn.com>
2019-10-22 11:16:02 +0300

    Refactor to expose a test endpoint

--- src/main/java/com/eurodyn/qlack/test/webdesktop/QlackTestWebdesktopApplication.java
+++ src/main/java/com/eurodyn/qlack/test/webdesktop/QlackTestWebdesktopApplication.java
@@ -3,23 +3,15 @@ package com.eurodyn.qlack.test.webdesktop;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import lombok.extern.java.Log;
 
 /**
  * Spring boot application class
  *
  * @author European Dynamics SA.
  */
+@Log
 @SpringBootApplication
-@EnableJpaRepositories({"com.eurodyn.qlack.webdesktop.repository"})
-@EntityScan({"com.eurodyn.qlack.webdesktop.model"})
-@ComponentScan(basePackages = {
-        "com.eurodyn.qlack.webdesktop.configuration",
-        "com.eurodyn.qlack.webdesktop.mapper",
-        "com.eurodyn.qlack.webdesktop.service"
-       })
 public class QlackTestWebdesktopApplication {
 
     public static void main(String[] args) {
